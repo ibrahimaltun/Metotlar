adca229c44a4b41e057a56ac098c4a2fbec5c248
ibrahim <53177796+ibrahimaltun@users.noreply.github.com>
2021-01-16 23:42:19 +0300

    Proje dosyası ekle.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2};
+            
+            // type-safe --- tip güvenli
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("---------------");
+            }
+
+            Console.WriteLine("--------------Metotlar-------------");
+
+            // instance -- bir class örneği oluşturma işlemidir.
+            // encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Elma","Amasya",9,345);
+            sepetManager.Ekle2("Armut","Çankırı",12,435);
+
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //syntax
+        //naming convention
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Ürün Sepete Eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Ürün Sepete Eklendi : " + urunAdi);
+        }
+    }
+}
